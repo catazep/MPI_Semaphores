8b49f57ae71bea093e625b664f85a26c773cf4b2
Georgescu Catalin <catazep@yahoo.com>
2018-02-04 19:30:50 +0200

    8 toggle

--- Semaphores/Semaphores.cpp
+++ Semaphores/Semaphores.cpp
@@ -5,30 +5,64 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-	
+
 	MPI_Init(NULL, NULL);
 
 	int world_rank, world_size;
 	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-	int semaphore, toggle = 0, duration = 0;
+	int semaphore, leftSemaphore, toggle = 0;
+
+
 
 	while (true)
 	{
 
 		if (world_rank == 0) {
-			Sleep(2000);
-			toggle = (toggle + 1) % 2;
+			if (toggle == 0 || toggle == 2)
+			{
+				Sleep(4500);
+			}
+			else if (toggle == 1 || toggle == 3)
+			{
+				Sleep(1500);
+			}
+			toggle = (toggle + 1) % 4;
 		}
 
 		// Broadcast all tasks
 		MPI_Bcast(&toggle, 1, MPI_INT, 0, MPI_COMM_WORLD);
-		semaphore = (world_rank + toggle) % 2;
+
+		if (toggle < 2)
+		{
+			if (world_rank % 2 == 0)
+			{
+				semaphore = toggle % 2;
+				leftSemaphore = !semaphore;
+			}
+			else
+			{
+				semaphore = 0;
+				leftSemaphore = 0;
+			}
+		}
+		else
+		{
+			if (world_rank % 2 == 1)
+			{
+				semaphore = toggle % 2;
+				leftSemaphore = !semaphore;
+			}
+			else
+			{
+				semaphore = 0;
+				leftSemaphore = 0;
+			}
+		}
 		MPI_Barrier(MPI_COMM_WORLD);
 		// Wait all tasks
-		cout << "Process " << world_rank << " is " << semaphore << endl;
-
+		cout << "Process " << world_rank << " is " << semaphore << " - " << leftSemaphore;
 		if (world_rank == 0)
 		{
 			cout << endl;
